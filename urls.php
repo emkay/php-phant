954db5965b2fa41645789aaabb28822eca9043d8
Michael Matuzak <mmatuzak@gmail.com>
2009-01-14 05:34:13 +0000

    cleaning up directory structure

--- urls.php
+++
@@ -1,7 +0,0 @@
-<?php
-$_URLS = array('http://news.ycombinator.com', 'Ycombinator',
-		'http://reddit.com', 'Reddit',
-		'http://news.google.com', 'Google News',
-		'http://news.yahoo.com', 'Yahoo News'
-);
-?>
